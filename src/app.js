2741fa09e19b69551273d20569b60ef06b8a33dc
Akvile4 <aqua0818@gmail.com>
2022-02-02 17:27:22 +0200

    added read method from CRUD

--- src/app.js
+++ src/app.js
@@ -7,13 +7,28 @@ const app = async (yargsObj) => {
     try {
         const collection = await connection();
         if (yargsObj.add) {
-            // add movie to mongodb database, needs collection and success message
+                // add movie to mongodb database, needs collection and success message
             await addMovie(collection, {
                 title: yargsObj.title, 
                 actor: yargsObj.actor,
             });
-        } else {
-            console.log("incorrect command");
+        } 
+        else if (yargsObj.read) {
+                // we ask MondoDB to find our Cllection named "Movies" (if there is none it will create for us)
+            const database = client.db("Movies");
+                // we ask MongoDB to access the child in our collection (-"- will create one)
+            const collection = database.collection("Movie");
+                // we asking to find what is in {}
+            const findThis = { title: "The Dressmaker", actor: "Kate Winslet"};
+                // we using findOne method
+            const foundThis = await collection.findOne(findThis);
+                // print it in the terminal
+            console.log(foundThis);
+        }
+        
+        
+        else {
+            console.log("That is not the right command");
         }
         client.close();
     } catch (error) {
